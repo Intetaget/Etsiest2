500dc59eeaa53e867a95d7ca62a5b4213b70217f
Mans Nilsson <intetaget>
2016-03-09 15:54:19 -0500

    Added api call and set the key also set the folders for erb and stylingsheets

--- lib/etsiest2.rb
+++ lib/etsiest2.rb
@@ -1,5 +1,19 @@
+require 'pry'
+require "sinatra/base"
+require "etsy"
 require "etsiest2/version"
 
+#according to instructions, it is outside the module
+Etsy.api_key = ENV["ETSY_KEY"]
+# run this in the shell "ETSY_KEY=foobarbaz bundle exec ruby lib/etsiest.rb"
+
 module Etsiest2
-  # Your code goes here...
+  # Your API call goes here...
+  	class App < Sinatra::Base
+
+ 		get "/" do
+      		response=Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'death')
+		  	erb :index, locals: { results: response.result }
+      	end
+
 end
