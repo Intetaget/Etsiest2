2fd4887ff550c76406a0c9e66b4fb391a138b8cd
Mans Nilsson <intetaget>
2016-03-09 16:14:34 -0500

    needed the starterline

--- lib/etsiest2.rb
+++ lib/etsiest2.rb
@@ -8,12 +8,12 @@ Etsy.api_key = ENV["ETSY_KEY"]
 # run this in the shell "ETSY_KEY=foobarbaz bundle exec ruby lib/etsiest.rb"
 
 module Etsiest2
-  # Your API call goes here...
+  # Your API call goes here...It reaches for the data from etsy.
   	class App < Sinatra::Base
 
  		get "/" do
       		response=Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'death')
 		  	erb :index, locals: { results: response.result }
       	end
-
-end
+      	run! if app_file == $0
+end		#the above line is magic
